e4c75c499cc6faf56593c54461e3ccc2236a2821
hfphw196 <131566672+hfphw196@users.noreply.github.com>
2023-12-15 11:13:03 +0700

    Configure DB & Redis variables

--- admin/config.php
+++ admin/config.php
@@ -1,6 +1,6 @@
 <?php
-define("DB_HOST", "localhost");
-define("DB_USER", "root");
-define("DB_PASS", "");
-define("DB_NAME", "website_footballkit");
+define("AZURE_MYSQL_HOST", "localhost");
+define("AZURE_MYSQL_USERNAME", "root");
+define("AZURE_MYSQL_PASSWORD", "");
+define("AZURE_MYSQL_DBNAME", "website_footballkit");
 ?>
